710c742754110fe7ac3f9ac89cadc3a8505ee7c8
Alvaro Enrique <alvaroenriquehl@gmail.com>
2017-10-01 19:07:18 -0500

    css de login y registro casi terminado

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-var no_coin = function(){
-    
+var no_coin = function(){   
     var nodo_login = document.getElementById("login");
     var nodo_ingresar = document.getElementById("ingresar");
-    var n_span = document.createElement("span");
-    n_span.textContent = "usuario incorrecto";
-    nodo_login.insertBefore(n_span, nodo_ingresar);
     nodo_ingresar.classList.remove("mt-4")
+    var n_span = document.createElement("span");
+    n_span.textContent = "Usuario y/o contraseña no coinciden";
+    nodo_login.insertBefore(n_span, nodo_ingresar);  
 }
-var hola_kazu = function(){
-    console.log("HOLA MUNDO KAZU")
+
+var abrir_registro = function(){
+    var modal = document.getElementById("mimodal");
+    modal.classList.remove("invisible");
+    modal.classList.add("visible");
+    
+}
+var cerrar_registro = function(){
+    var modal = document.getElementById("mimodal");
+    modal.classList.remove("visible");
+    modal.classList.add("invisible");
 }
 
 var main = function(){
+    
+    document.getElementById("crear_usuario").addEventListener("click", abrir_registro)
 
 }
 
 window.onload = main;
-=======
->>>>>>> parent of 77c26ae... login v1.2
-=======
->>>>>>> parent of c279bc9... version 1.0
