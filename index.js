9769a176c10c99716b76467c134ab1bd88121d44
Alvaro Enrique <alvaroenriquehl@gmail.com>
2017-10-05 17:48:52 -0500

    mejorado el estilo del login

--- index.js
+++ index.js
@@ -1,261 +1,269 @@
-var abrir_registro = function () {
-  var modal = document.getElementById("mimodal");
-  modal.classList.remove("invisible");
-  modal.classList.add("visible");
-  modal.classList.add("animated");
-  modal.classList.add("bounceInDown");
-  modal.addEventListener("animationend", function () {
-    modal.classList.remove("animated");
-    modal.classList.remove("bounceInDown");
-  })
+var abrir_registro = function() {
+    var modal = document.getElementById("mimodal");
+    modal.classList.remove("invisible");
+    modal.classList.add("visible");
+    modal.classList.add("animated");
+    modal.classList.add("bounceInDown");
+    modal.addEventListener("animationend", function() {
+        modal.classList.remove("animated");
+        modal.classList.remove("bounceInDown");
+    })
 }
-var cerrar_registro = function () {
-  var modal = document.getElementById("mimodal");
-  modal.classList.remove("visible");
-  modal.classList.add("invisible");
+var cerrar_registro = function() {
+    var modal = document.getElementById("mimodal");
+
+    modal.classList.add("animated");
+    modal.classList.add("bounceOut");
+    modal.addEventListener("animationend", function() {
+        if (modal.classList.contains("bounceOut")) {
+            modal.classList.remove("visible");
+            modal.classList.add("invisible");
+            modal.classList.remove("animated");
+            modal.classList.remove("bounceOut");
+        }
+
+    })
 }
 
-var habilitar_botones = function () {
-  var condicion = document.getElementById("input0").checked;
-  var a = document.getElementById("input1");
-  var b = document.getElementById("input2");
-  var c = document.getElementById("input3");
-  var d = document.getElementById("input4");
-  var e = document.getElementById("input5");
-  if (condicion == false) {
-    a.disabled = true;
-    b.disabled = true;
-    c.disabled = true;
-    d.disabled = true;
-    e.disabled = true;
-  }
-  else {
-    a.disabled = false;
-    b.disabled = false;
-    c.disabled = false;
-    d.disabled = false;
-    e.disabled = false;
-  }
+var habilitar_botones = function() {
+    var condicion = document.getElementById("input0").checked;
+    var a = document.getElementById("input1");
+    var b = document.getElementById("input2");
+    var c = document.getElementById("input3");
+    var d = document.getElementById("input4");
+    var e = document.getElementById("input5");
+    if (condicion == false) {
+        a.disabled = true;
+        b.disabled = true;
+        c.disabled = true;
+        d.disabled = true;
+        e.disabled = true;
+    } else {
+        a.disabled = false;
+        b.disabled = false;
+        c.disabled = false;
+        d.disabled = false;
+        e.disabled = false;
+    }
 };
 
 function validarQueEsEmail(email) {
-  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(email);
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
 };
 
 function validateEmail() {
-  var email = document.getElementById("input1").value;
-  var resultado = document.getElementById("result");
-  resultado.classList.add("texto_rojo")
-  var jsRequest = {
-    "correo": email
-  };
-  if (validarQueEsEmail(email)) {
-    resultado.innerHTML = ""; resultado.value = "";
-    console.log("cagadox2");
-    console.log(JSON.stringify(jsRequest));
-    //Iniciamos la comunicacion con el servidor
-    var url = "http://45.55.64.102/g2/usuario/correo_repetido";
-    var req = new XMLHttpRequest();
-    req.open("POST", url);
-    req.send(JSON.stringify(jsRequest));
-    req.onreadystatechange = respuesta;
-  }
-  else {
-    resultado.innerHTML = "no es un correo valido";
-  }
+    var email = document.getElementById("input1").value;
+    var resultado = document.getElementById("result");
+    resultado.classList.add("texto_rojo")
+    var jsRequest = {
+        "correo": email
+    };
+    if (validarQueEsEmail(email)) {
+        resultado.innerHTML = "";
+        resultado.value = "";
+        console.log("cagadox2");
+        console.log(JSON.stringify(jsRequest));
+        //Iniciamos la comunicacion con el servidor
+        var url = "http://45.55.64.102/g2/usuario/correo_repetido";
+        var req = new XMLHttpRequest();
+        req.open("POST", url);
+        req.send(JSON.stringify(jsRequest));
+        req.onreadystatechange = respuesta;
+    } else {
+        resultado.innerHTML = "no es un correo valido";
+    }
 };
 
 function validateUser() {
-  var user = document.getElementById("input3").value;
-  var resultado = document.getElementById("result3").value;
-  var jsRequest = {
-    "usuario": user
-  };
-  console.log(JSON.stringify(jsRequest));
-  //Iniciamos la comunicacion con el servidor
-  var url = "http://45.55.64.102/g2/usuario/usuario_repetido";
-  var req = new XMLHttpRequest();
-  req.open("POST", url);
-  req.send(JSON.stringify(jsRequest));
-  req.onreadystatechange = respuesta1;
+    var user = document.getElementById("input3").value;
+    var resultado = document.getElementById("result3").value;
+    var jsRequest = {
+        "usuario": user
+    };
+    console.log(JSON.stringify(jsRequest));
+    //Iniciamos la comunicacion con el servidor
+    var url = "http://45.55.64.102/g2/usuario/usuario_repetido";
+    var req = new XMLHttpRequest();
+    req.open("POST", url);
+    req.send(JSON.stringify(jsRequest));
+    req.onreadystatechange = respuesta1;
 }
 //mejorarlo con respuesta1
 function respuesta(evt) {
-  var resultado = document.getElementById("result");
-  if (this.readyState == 4 && this.status == 200) {
-    console.log(evt.target.responseText);
-    var respuesta = JSON.parse(evt.target.responseText);
-    if (respuesta.mensaje[0] == 0) {
-      resultado.innerHTML = ""; resultado.value = "";
-      console.log("cagado");
-    } else if (respuesta.mensaje[0] == 1) {
-      resultado.innerHTML = "correo ingresado existe";
+    var resultado = document.getElementById("result");
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(evt.target.responseText);
+        var respuesta = JSON.parse(evt.target.responseText);
+        if (respuesta.mensaje[0] == 0) {
+            resultado.innerHTML = "";
+            resultado.value = "";
+            console.log("cagado");
+        } else if (respuesta.mensaje[0] == 1) {
+            resultado.innerHTML = "correo ingresado existe";
+        }
     }
-  }
 };
 
 function respuesta1(evt) {
-  var resultado = document.getElementById("result3");
-  if (this.readyState == 4 && this.status == 200) {
-    console.log(evt.target.responseText);
-    var respuesta = JSON.parse(evt.target.responseText);
-    if (respuesta.mensaje[0] == 0) {
-      resultado.innerHTML = ""; resultado.value = "";
-      console.log("xd");
-    } else if (respuesta.mensaje[0] == 1) {
-      resultado.innerHTML = "Usuario ya existente";
+    var resultado = document.getElementById("result3");
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(evt.target.responseText);
+        var respuesta = JSON.parse(evt.target.responseText);
+        if (respuesta.mensaje[0] == 0) {
+            resultado.innerHTML = "";
+            resultado.value = "";
+            console.log("xd");
+        } else if (respuesta.mensaje[0] == 1) {
+            resultado.innerHTML = "Usuario ya existente";
+        }
     }
-  }
 };
 
 function repetirCorreo() {
-  var correo = document.getElementById("input1").value;
-  var correo_repetido = document.getElementById("input2").value;
-  var resultado = document.getElementById("result2");
-  resultado.classList.add("texto_rojo");
-  if (correo != correo_repetido) {
-    resultado.innerHTML = "Correo ingresado no coincide";
-  }
-  else {
-    resultado.innerHTML = ""; resultado.value = "";
-  }
+    var correo = document.getElementById("input1").value;
+    var correo_repetido = document.getElementById("input2").value;
+    var resultado = document.getElementById("result2");
+    resultado.classList.add("texto_rojo");
+    if (correo != correo_repetido) {
+        resultado.innerHTML = "Correo ingresado no coincide";
+    } else {
+        resultado.innerHTML = "";
+        resultado.value = "";
+    }
 };
 
 function repetirContraseña() {
-  var contraseña = document.getElementById("input4").value;
-  var confContraseña = document.getElementById("input5").value;
-  var resultado = document.getElementById("result4");
-  resultado.classList.add("texto_rojo");
-  if (contraseña == confContraseña) {
-    resultado.innerHTML = ""; resultado.value = "";
-  }
-  else {
-    resultado.innerHTML = "Contraseña ingresada no coincide";
-  }
-};
-
-function cifrarPassword(){
-  var password = document.getElementById("input4").value;
-  var jsRequest = {
-    "texto" : password
-  };
-  var xhr = new XMLHttpRequest();
-  var url = "http://45.55.64.102/g2/cipher/encode";
-  xhr.open("POST", url);
-  xhr.onreadystatechange = respuestaPassword;
-  xhr.send(JSON.stringify(jsRequest));
-};
-
-
-function respuestaPassword(evt){
-  if (evt.target.readyState == 4 && evt.target.status == 200) {
-    var password = JSON.parse(evt.target.responseText);
-    var cifrado = password.mensaje[0];
-    console.log(cifrado);
-
-    var correo=document.getElementById("input1").value;
-    var user=document.getElementById("input3").value;
-
-    var jsRequest1 = {
-      "usuario" : user,
-      "correo" : correo,
-      "contrasenia" : cifrado
-    };
-
-    console.log(JSON.stringify(jsRequest1));
-    var req1 = new XMLHttpRequest();
-    var url1 = "http://45.55.64.102/g2/usuario/guardar";
-    req1.open("POST",url1);
-    req1.onreadystatechange = respuestaAddTodo;
-    console.log("ESTO ES LO QUE SE ESTA MANDANDO COMO FORMULARIO");
-    req1.send(JSON.stringify(jsRequest1));
-  }
-};
-
-
-var respuestaAddTodo = function (evt) {
-  if (evt.target.readyState == 4) {
-    if (evt.target.status == 200) {
-      // Todo OK en la comunicacion
-      console.log(evt.target.responseText);
+    var contraseña = document.getElementById("input4").value;
+    var confContraseña = document.getElementById("input5").value;
+    var resultado = document.getElementById("result4");
+    resultado.classList.add("texto_rojo");
+    if (contraseña == confContraseña) {
+        resultado.innerHTML = "";
+        resultado.value = "";
+    } else {
+        resultado.innerHTML = "Contraseña ingresada no coincide";
     }
-  }
-
 };
 
-function login(){
-  var password = document.getElementById("password_login").value;
-  var jsRequest = {
-    "texto" : password
-  };
-  var xhr = new XMLHttpRequest();
-  var url = "http://45.55.64.102/g2/cipher/encode";
-  xhr.open("POST", url);
-  xhr.onreadystatechange = verificarPassword;
-  xhr.send(JSON.stringify(jsRequest));
-};
-
-function verificarPassword(evt){
-  if (evt.target.readyState == 4 && evt.target.status == 200) {
-    var password = JSON.parse(evt.target.responseText);
-    var cifrado = password.mensaje[0];
-    console.log(cifrado);
-    var usuario = document.getElementById("usuario_login").value;
+function cifrarPassword() {
+    var password = document.getElementById("input4").value;
     var jsRequest = {
-      "usuario" : usuario,
-      "contrasenia" : cifrado
+        "texto": password
     };
-    console.log(JSON.stringify(jsRequest));
-    var url = "http://45.55.64.102/g2/usuario/validar";
-    var req = new XMLHttpRequest();
-    req.open("POST", url);
-    req.onreadystatechange = redireccion;
-    req.send(JSON.stringify(jsRequest));
-  }
+    var xhr = new XMLHttpRequest();
+    var url = "http://45.55.64.102/g2/cipher/encode";
+    xhr.open("POST", url);
+    xhr.onreadystatechange = respuestaPassword;
+    xhr.send(JSON.stringify(jsRequest));
 };
 
-function redireccion(evt){
-  if (evt.target.readyState == 4 && evt.target.status == 200) {
-    console.log(evt.target.responseText);
-    var respuesta = JSON.parse(evt.target.responseText);
-    if (respuesta.mensaje[0] == 1) {
-      document.getElementById("result5").innerHTML = "Login OK";//Listo
-      window.location.href = "mantanimiento.html";
-    }else{
-      document.getElementById("result5").innerHTML = "Usuario y/o contraseña no coinciden";
+
+function respuestaPassword(evt) {
+    if (evt.target.readyState == 4 && evt.target.status == 200) {
+        var password = JSON.parse(evt.target.responseText);
+        var cifrado = password.mensaje[0];
+        console.log(cifrado);
+
+        var correo = document.getElementById("input1").value;
+        var user = document.getElementById("input3").value;
+
+        var jsRequest1 = {
+            "usuario": user,
+            "correo": correo,
+            "contrasenia": cifrado
+        };
+
+        console.log(JSON.stringify(jsRequest1));
+        var req1 = new XMLHttpRequest();
+        var url1 = "http://45.55.64.102/g2/usuario/guardar";
+        req1.open("POST", url1);
+        req1.onreadystatechange = respuestaAddTodo;
+        console.log("ESTO ES LO QUE SE ESTA MANDANDO COMO FORMULARIO");
+        req1.send(JSON.stringify(jsRequest1));
     }
-  }
+};
+
+
+var respuestaAddTodo = function(evt) {
+    if (evt.target.readyState == 4) {
+        if (evt.target.status == 200) {
+            // Todo OK en la comunicacion
+            console.log(evt.target.responseText);
+        }
+    }
+
+};
+
+function login() {
+    var password = document.getElementById("password_login").value;
+    var jsRequest = {
+        "texto": password
+    };
+    var xhr = new XMLHttpRequest();
+    var url = "http://45.55.64.102/g2/cipher/encode";
+    xhr.open("POST", url);
+    xhr.onreadystatechange = verificarPassword;
+    xhr.send(JSON.stringify(jsRequest));
+};
+
+function verificarPassword(evt) {
+    if (evt.target.readyState == 4 && evt.target.status == 200) {
+        var password = JSON.parse(evt.target.responseText);
+        var cifrado = password.mensaje[0];
+        console.log(cifrado);
+        var usuario = document.getElementById("usuario_login").value;
+        var jsRequest = {
+            "usuario": usuario,
+            "contrasenia": cifrado
+        };
+        console.log(JSON.stringify(jsRequest));
+        var url = "http://45.55.64.102/g2/usuario/validar";
+        var req = new XMLHttpRequest();
+        req.open("POST", url);
+        req.onreadystatechange = redireccion;
+        req.send(JSON.stringify(jsRequest));
+    }
+};
+
+function redireccion(evt) {
+    if (evt.target.readyState == 4 && evt.target.status == 200) {
+        console.log(evt.target.responseText);
+        var respuesta = JSON.parse(evt.target.responseText);
+        if (respuesta.mensaje[0] == 1) {
+            document.getElementById("result5").innerHTML = "Login OK"; //Listo
+            window.location.href = "mantanimiento.html";
+        } else {
+            document.getElementById("result5").innerHTML = "Usuario y/o contraseña no coinciden";
+        }
+    }
 }
 
-function restringirRegistro()
-{
-  var a=document.getElementById("result").value;
-  var b=document.getElementById("result2").value;
-  var c=document.getElementById("result3").value;
-  var d=document.getElementById("result4").value;
-  var boton=document.getElementById("guar_camb");
-  if(a==""&&b==""&&c==""&&d=="")
-  {
-    boton.disabled=false;
-  }
-  else{
-    boton.disabled=true;
-  }
+function restringirRegistro() {
+    var a = document.getElementById("result").value;
+    var b = document.getElementById("result2").value;
+    var c = document.getElementById("result3").value;
+    var d = document.getElementById("result4").value;
+    var boton = document.getElementById("guar_camb");
+    if (a == "" && b == "" && c == "" && d == "") {
+        boton.disabled = false;
+    } else {
+        boton.disabled = true;
+    }
 }
 
-var main = function () {
-  document.getElementById("crear_usuario").addEventListener("click", abrir_registro);
+var main = function() {
+    document.getElementById("crear_usuario").addEventListener("click", abrir_registro);
     document.getElementById("icon_cerrar").addEventListener("click", cerrar_registro);
     document.getElementById("guar_camb").addEventListener("click", cerrar_registro);
     document.addEventListener("keyup", restringirRegistro);
-    document.getElementById("input1").addEventListener("keyup",validateEmail);
+    document.getElementById("input1").addEventListener("keyup", validateEmail);
     document.addEventListener("click", habilitar_botones);
-    document.getElementById("input2").addEventListener("keyup",repetirCorreo);
-    document.getElementById("input3").addEventListener("keyup",validateUser);
-    document.getElementById("input5").addEventListener("keyup",repetirContraseña);
-    document.getElementById("guar_camb").addEventListener("click",cifrarPassword);
-    document.getElementById("ingresar").addEventListener("click",login);
+    document.getElementById("input2").addEventListener("keyup", repetirCorreo);
+    document.getElementById("input3").addEventListener("keyup", validateUser);
+    document.getElementById("input5").addEventListener("keyup", repetirContraseña);
+    document.getElementById("guar_camb").addEventListener("click", cifrarPassword);
+    document.getElementById("ingresar").addEventListener("click", login);
 }
 
 window.onload = main;
