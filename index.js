86406cce1f546751c110d9df4e318b883656d9c5
kazuichi123 <kazuichi96@hotmail.com>
2017-10-06 00:39:33 -0500

    login finalizado

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+//esta funcion abre el formulario de creacion presionando el boton "crear_usuario"
 var abrir_registro = function() {
+  //si ya creaste un usuario se ponen en blanco todos los campos del formulario
+    document.getElementById("input1").value='';
+    document.getElementById("input2").value='';
+    document.getElementById("input3").value='';
+    document.getElementById("input4").value='';
+    document.getElementById("input5").value='';
+    //vuelve visible el formulario
     var modal = document.getElementById("mimodal");
     modal.classList.remove("invisible");
     modal.classList.add("visible");
+    //animaciones
     modal.classList.add("animated");
     modal.classList.add("bounceInDown");
     modal.addEventListener("animationend", function() {
@@ -9,11 +18,14 @@ var abrir_registro = function() {
         modal.classList.remove("bounceInDown");
     })
 }
+
+//presionando el boton x, se vuelve invisible el modal
 var cerrar_registro = function() {
     var modal = document.getElementById("mimodal");
-
+    //animacion de cierre
     modal.classList.add("animated");
     modal.classList.add("bounceOut");
+    //funcion para crear la animacion y volver invisible el modal
     modal.addEventListener("animationend", function() {
         if (modal.classList.contains("bounceOut")) {
             modal.classList.remove("visible");
@@ -24,7 +36,7 @@ var cerrar_registro = function() {
 
     })
 }
-
+//si el boton de condiciones para el registro no se activa, los campos para rellenar tampoco
 var habilitar_botones = function() {
     var condicion = document.getElementById("input0").checked;
     var a = document.getElementById("input1");
@@ -47,11 +59,13 @@ var habilitar_botones = function() {
     }
 };
 
+//sacado de internet para validar que es un Email
 function validarQueEsEmail(email) {
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 };
 
+//funcion que te dice si el email ingresado en el campo ya existe en la base de datos
 function validateEmail() {
     var email = document.getElementById("input1").value;
     var resultado = document.getElementById("result");
@@ -59,22 +73,29 @@ function validateEmail() {
     var jsRequest = {
         "correo": email
     };
+    //se llama a la funcion validar email para que no acepte nada que no tenga formato email
     if (validarQueEsEmail(email)) {
+        //se vuele vacio el campo y valor de resultado
         resultado.innerHTML = "";
         resultado.value = "";
-        console.log("cagadox2");
+        //te pinta en la consola lo que vas a mandar a correo_repetido
         console.log(JSON.stringify(jsRequest));
         //Iniciamos la comunicacion con el servidor
         var url = "http://45.55.64.102/g2/usuario/correo_repetido";
+        //creacion un objeto para intercambiar informacion con el servidor
         var req = new XMLHttpRequest();
+        //abre el servidor
         req.open("POST", url);
+        //manda la informacion al servidor
         req.send(JSON.stringify(jsRequest));
+        //define una funcion que validara la respuesta y estado completado
         req.onreadystatechange = respuesta;
     } else {
         resultado.innerHTML = "no es un correo valido";
     }
 };
 
+//mismo que la funcion de validar email pero con usuario
 function validateUser() {
     var user = document.getElementById("input3").value;
     var resultado = document.getElementById("result3").value;
@@ -82,49 +103,52 @@ function validateUser() {
         "usuario": user
     };
     console.log(JSON.stringify(jsRequest));
-    //Iniciamos la comunicacion con el servidor
     var url = "http://45.55.64.102/g2/usuario/usuario_repetido";
     var req = new XMLHttpRequest();
     req.open("POST", url);
     req.send(JSON.stringify(jsRequest));
     req.onreadystatechange = respuesta1;
 }
-//mejorarlo con respuesta1
+
+//funcion para validar la respuesta y estado
 function respuesta(evt) {
     var resultado = document.getElementById("result");
     if (this.readyState == 4 && this.status == 200) {
         console.log(evt.target.responseText);
         var respuesta = JSON.parse(evt.target.responseText);
+        //condicional para que no se repitan correos existentes
         if (respuesta.mensaje[0] == 0) {
             resultado.innerHTML = "";
             resultado.value = "";
-            console.log("cagado");
         } else if (respuesta.mensaje[0] == 1) {
             resultado.innerHTML = "correo ingresado existe";
         }
     }
 };
 
+//funcion para validar la respuesta y estado (usuario)
 function respuesta1(evt) {
     var resultado = document.getElementById("result3");
     if (this.readyState == 4 && this.status == 200) {
         console.log(evt.target.responseText);
         var respuesta = JSON.parse(evt.target.responseText);
+        //condicional para que no se repitan usuarios ya existentes
         if (respuesta.mensaje[0] == 0) {
             resultado.innerHTML = "";
             resultado.value = "";
-            console.log("xd");
         } else if (respuesta.mensaje[0] == 1) {
             resultado.innerHTML = "Usuario ya existente";
         }
     }
 };
 
+//funcion para validar que se repitio bien el correo existente
 function repetirCorreo() {
     var correo = document.getElementById("input1").value;
     var correo_repetido = document.getElementById("input2").value;
     var resultado = document.getElementById("result2");
     resultado.classList.add("texto_rojo");
+    //´pinta que el correo no coincide hasta que acierte con el otro
     if (correo != correo_repetido) {
         resultado.innerHTML = "Correo ingresado no coincide";
     } else {
@@ -133,11 +157,13 @@ function repetirCorreo() {
     }
 };
 
+//funcion para validar que se repitio bien la contraseña
 function repetirContraseña() {
     var contraseña = document.getElementById("input4").value;
     var confContraseña = document.getElementById("input5").value;
     var resultado = document.getElementById("result4");
     resultado.classList.add("texto_rojo");
+    //´pinta que la contraseña no coincide hasta que acierte con la otro
     if (contraseña == confContraseña) {
         resultado.innerHTML = "";
         resultado.value = "";
@@ -146,6 +172,7 @@ function repetirContraseña() {
     }
 };
 
+//funcion que encripta el password mediante el metodo dado al presionar el boton guar_camb
 function cifrarPassword() {
     var password = document.getElementById("input4").value;
     var jsRequest = {
@@ -163,6 +190,7 @@ function respuestaPassword(evt) {
     if (evt.target.readyState == 4 && evt.target.status == 200) {
         var password = JSON.parse(evt.target.responseText);
         var cifrado = password.mensaje[0];
+        //pinta en la consola la contraseña cifrada
         console.log(cifrado);
 
         var correo = document.getElementById("input1").value;
@@ -173,7 +201,7 @@ function respuestaPassword(evt) {
             "correo": correo,
             "contrasenia": cifrado
         };
-
+        //pinta la informacion del usuario que se guardara en la base de datos
         console.log(JSON.stringify(jsRequest1));
         var req1 = new XMLHttpRequest();
         var url1 = "http://45.55.64.102/g2/usuario/guardar";
@@ -184,7 +212,7 @@ function respuestaPassword(evt) {
     }
 };
 
-
+//funcion para pintar que se realizo correctamente el guardado del nuevo usuario
 var respuestaAddTodo = function(evt) {
     if (evt.target.readyState == 4) {
         if (evt.target.status == 200) {
@@ -192,9 +220,10 @@ var respuestaAddTodo = function(evt) {
             console.log(evt.target.responseText);
         }
     }
-
 };
 
+
+//funcion que verifica si el usuario esta en la base de datos para otorgarle el acceso a la pagina web
 function login() {
     var password = document.getElementById("password_login").value;
     var jsRequest = {
@@ -207,6 +236,7 @@ function login() {
     xhr.send(JSON.stringify(jsRequest));
 };
 
+//funcion que verifica la contraseña del usuario
 function verificarPassword(evt) {
     if (evt.target.readyState == 4 && evt.target.status == 200) {
         var password = JSON.parse(evt.target.responseText);
@@ -226,6 +256,7 @@ function verificarPassword(evt) {
     }
 };
 
+//funcion que redirige a la pagina web mantanimiento.
 function redireccion(evt) {
     if (evt.target.readyState == 4 && evt.target.status == 200) {
         console.log(evt.target.responseText);
@@ -239,31 +270,33 @@ function redireccion(evt) {
     }
 }
 
+//funcion que restringe mandar informacion a la base de datos si los campos no estan correctamente terminados
 function restringirRegistro() {
     var a = document.getElementById("result").value;
     var b = document.getElementById("result2").value;
     var c = document.getElementById("result3").value;
     var d = document.getElementById("result4").value;
     var boton = document.getElementById("guar_camb");
+    //si todos los campos no presenta error se habilita el guardado
     if (a == "" && b == "" && c == "" && d == "") {
         boton.disabled = false;
     } else {
         boton.disabled = true;
     }
 }
-
+//funcion main donde se llama a todas las funciones mediante eventos
 var main = function() {
+  document.getElementById("ingresar").addEventListener("click", login);
     document.getElementById("crear_usuario").addEventListener("click", abrir_registro);
     document.getElementById("icon_cerrar").addEventListener("click", cerrar_registro);
     document.getElementById("guar_camb").addEventListener("click", cerrar_registro);
-    document.addEventListener("keyup", restringirRegistro);
+    document.getElementById("guar_camb").addEventListener("click", cifrarPassword);
     document.getElementById("input1").addEventListener("keyup", validateEmail);
-    document.addEventListener("click", habilitar_botones);
     document.getElementById("input2").addEventListener("keyup", repetirCorreo);
     document.getElementById("input3").addEventListener("keyup", validateUser);
     document.getElementById("input5").addEventListener("keyup", repetirContraseña);
-    document.getElementById("guar_camb").addEventListener("click", cifrarPassword);
-    document.getElementById("ingresar").addEventListener("click", login);
+    document.addEventListener("keyup", restringirRegistro);
+    document.addEventListener("click", habilitar_botones);
 }
 
 window.onload = main;
--- mantenimiento.js
+++ mantenimiento.js
@@ -5,7 +5,6 @@ var crear_tablas_nombre = function (lista_db, nodo_id, i) {
     var dep_td = document.createElement("td");
     depar.appendChild(dep_tr);
     dep_td.textContent = lista_db.nombre;
-    //console.log(lista[i].id);
     dep_tr.appendChild(dep_td);
     var iconos = document.createElement("td");
     var i_search = document.createElement("i");
@@ -17,22 +16,26 @@ var crear_tablas_nombre = function (lista_db, nodo_id, i) {
         if(nodo_id == "departamentos"){
             if(document.getElementById("provincias").childNodes.length == 0){
                 mostrar_prov(i + 1);
+                    console.log(i);
             }
             else{
                 eliminar_nodos("provincias")
                 eliminar_nodos("prov_head")
             }
         }
+
         if(nodo_id == "provincias"){
             if(document.getElementById("distritos").childNodes.length == 0){
-                mostrar_distr(i + 1);
+                mostrar_dis(i+1);
+                console.log(i);
             }
             else{
                 eliminar_nodos("distritos")
-                eliminar_nodos("distr_head")
+                eliminar_nodos("dis_head")
             }
         }
-        
+
+
     })
     i_pencil.className = "fa fa-pencil ml-3 h_azul animated pulse";
     i_eliminar.className = "fa fa-times ml-3 h_rojo animated pulse";
@@ -52,7 +55,7 @@ var mostrar_dep = function () {
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             lista = JSON.parse(xhr.responseText);
-
+            console.log(lista);
             for (var i = 0; i < lista.length; i++) {
 
                 crear_tablas_nombre(lista[i], "departamentos", i)
@@ -63,13 +66,14 @@ var mostrar_dep = function () {
     xhr.open("GET", "http://45.55.64.102/g2/departamento/listar", true);
     xhr.send();
 }
+
 var mostrar_prov = function (depart_id) {
+
     var xhr = new XMLHttpRequest();
     var lista_prov = 0;
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
             lista_prov = JSON.parse(xhr.responseText);
-            //console.log(lista_prov);
             var nombre_head = document.createElement("th");
             nombre_head.textContent = "Nombre";
             var ope_head = document.createElement("th");
@@ -81,30 +85,34 @@ var mostrar_prov = function (depart_id) {
             }
         }
     }
-    xhr.open("GET", "http://45.55.64.102/g2/provincia/listar/" + depart_id, true);
+    xhr.open("GET","http://45.55.64.102/g2/provincia/listar/"+ depart_id, true);
     xhr.send();
 }
-var mostrar_distr = function (prov_id) {
+
+var mostrar_dis = function (prov_id) {
+  //console.log("2xxxx");
     var xhr = new XMLHttpRequest();
-    var lista_prov = 0;
+    var lista_dis = 0;
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            lista_prov = JSON.parse(xhr.responseText);
-            //console.log(lista_prov);
+              lista_dis = JSON.parse(xhr.responseText);
+            //console.log(lista_dis);
             var nombre_head = document.createElement("th");
             nombre_head.textContent = "Nombre";
             var ope_head = document.createElement("th");
             ope_head.textContent = "Operaciones";
-            document.getElementById("distr_head").appendChild(nombre_head);
-            document.getElementById("distr_head").appendChild(ope_head);
-            for (var i = 0; i < lista_prov.length; i++) {
-                crear_tablas_nombre(lista_prov[i], "distritos",i);
+            document.getElementById("dis_head").appendChild(nombre_head);
+            document.getElementById("dis_head").appendChild(ope_head);
+            for (var i = 0; i < lista_dis.length; i++) {
+                crear_tablas_nombre(lista_dis[i], "distritos",i);
+                console.log("hola");
             }
         }
     }
     xhr.open("GET", "http://45.55.64.102/g2/distrito/listar/" + prov_id, true);
     xhr.send();
 }
+
 var eliminar_nodos = function(nodo_id){
     var nodo = document.getElementById(nodo_id);
     while (nodo.firstChild) {
@@ -112,6 +120,18 @@ var eliminar_nodos = function(nodo_id){
     }
 }
 
+var respuestaAddDato = function(evt){
+  if(evt.target.readyState == 4 ){
+    if(evt.target.status == 200){
+      //se deberia poner la lista de departamentos, provincia o distrito
+      // en vez de 'lista'
+      console.log(evt.target);
+      document.getElementById('departamentos').innerHTML= "";
+      //--------------------------------------------------------
+    //  mostrar_dep();
+    }
+  }
+}
 
 
 
